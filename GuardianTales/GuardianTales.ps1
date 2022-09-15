2be160b8a638cc4789c7a623f16810883413604d
medaey <medaey@hotmail.com>
2022-09-15 04:33:57 +0200

    22.9.15.0 (#1)
    * fix crash script
    
    * Update GuardianTales.ps1
    
    * Update GuardianTales.exe
    
    * fix script

--- GuardianTales/GuardianTales.ps1
+++ GuardianTales/GuardianTales.ps1
@@ -1,38 +1,58 @@
 ## Start GuadiantTales Apps powered by BlueStack
 # Author : MedericCossu
 
-$loop = "True"
+class adb {
+    [string]return_adbDeviceAddress() {
+        $blueStackId = get-process -Name "HD-Player" |select -expand id -First 1
+        $adbDevicesPort = Get-NetTCPConnection -OwningProcess $blueStackId -LocalAddress 127.0.0.1 | select -expand LocalPort -First 1
+        $adbDeviceAddress = "127.0.0.1:$adbDevicesPort"
+        return $adbDeviceAddress
+    }
+    [int]return_appVsize() {
+        $appVsize = .\HD-Adb.exe -s "127.0.0.1:$(Get-NetTCPConnection -OwningProcess $(get-process -Name "HD-Player" |select -expand id -First 1) -LocalAddress 127.0.0.1 | select -expand LocalPort -First 1)" shell ps (.\HD-Adb.exe -s "127.0.0.1:$(Get-NetTCPConnection -OwningProcess $(get-process -Name "HD-Player" |select -expand id -First 1) -LocalAddress 127.0.0.1 | select -expand LocalPort -First 1)" shell pidof -s com.kakaogames.gdts) | ForEach-Object{($_ -split "\s+")[3]} | select -Last 1
+        return $appVsize
+    }
+}
 
 function Connect-adb{
-    $adbDeviceAddress = "127.0.0.1:$(Get-NetTCPConnection -OwningProcess $(get-process -Name "HD-Player" |select -expand id -First 1) -LocalAddress 127.0.0.1 | select -expand LocalPort -First 1)"
+    $AdbClass = New-Object -TypeName adb
     cd "C:\Program Files\BlueStacks_nxt"
-    ./HD-Adb.exe disconnect
-    ./HD-Adb.exe connect $adbDeviceAddress
+    ./HD-Adb.exe disconnect -out-null
+    ./HD-Adb.exe connect $AdbClass.return_adbDeviceAddress()
     Start-Sleep -Seconds 1
 }
 
 function Start-BlueStacks{
-    Start "C:\Program Files\BlueStacks_nxt\HD-Player.exe" -ArgumentList '--instance Nougat32_1 --cmd launchApp --package "com.kakaogames.gdts"'
-    Start-Sleep -Seconds 30
+    if((get-process "HD-Player" -ea SilentlyContinue) -eq $Null){ 
+        echo "Start BlueStacks..."
+        Start "C:\Program Files\BlueStacks_nxt\HD-Player.exe" -ArgumentList '--instance Nougat32_1 --cmd launchApp --package "com.kakaogames.gdts"'
+        Start-Sleep -Seconds 30
+    }
+    else{
+        echo "BlueStack is running"
+        Start-Sleep -Seconds 1
+    }
 }
+
+# Init var
+$loop = "True"
+$ErrorActionPreference = "SilentlyContinue"
+$AdbClass = New-Object -TypeName adb
 
 while($loop -eq "True")
 {
+    echo ""
     Start-BlueStacks
-    $adbDeviceAddress = "127.0.0.1:$(Get-NetTCPConnection -OwningProcess $(get-process -Name "HD-Player" |select -expand id -First 1) -LocalAddress 127.0.0.1 | select -expand LocalPort -First 1)"
     Connect-adb
-    $appVsize = .\HD-Adb.exe -s $adbDeviceAddress shell ps $(.\HD-Adb.exe -s $adbDeviceAddress shell pidof -s com.kakaogames.gdts) | ForEach-Object{($_ -split "\s+")[3]} | select -Last 1
-
-    If($appVsize -lt "2500000")
-    {
+    echo "GuardiansTailesVsize : $($AdbClass.return_appVsize())"
+    if($AdbClass.return_appVsize() -le 2500000){ 
+        echo "GuardiansTailes fail running"
         Stop-Process -Name "HD-Player"
-        Start-BlueStacks
-        Connect-adb
+        Start-Sleep -Seconds 2
     }
-    else
-    {
+    elseif($AdbClass.return_appVsize() -ge 2500000){ 
+        echo "Guardians is running"
         $loop = "False"
     }
-    exit
 }
-Start-Sleep -Seconds 10
+exit
