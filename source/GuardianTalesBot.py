9e072f51f036e1577e42f72b851d8195aae4e5e0
medaey <medaey@hotmail.com>
2022-09-25 05:37:25 +0200

    update fonction

--- source/GuardianTalesBot.py
+++ source/GuardianTalesBot.py
@@ -3,6 +3,7 @@ import time
 import psutil
 import easyocr
 from adbutils import adb
+import HomePage
 
 deviceAddress = "127.0.0.1:5555"
 def get_pid(processName):
@@ -18,38 +19,43 @@ def adb_find_app(deviceAddress, app):
     for i in range(0, len(adb.device(serial=deviceAddress).list_packages())):
         if adb.device(serial=deviceAddress).list_packages()[i] == app:
             appFound = 1
-    return appFound
-def adb_tap_string(string):
+    return
+def adb_find_string(string):
     time.sleep(1)
     adb.connect(deviceAddress)
-    adb.device(serial=deviceAddress).shell("screencap -p /sdcard/.screencap.png")
-    adb.device(serial=deviceAddress).sync.pull("/sdcard/.screencap.png", ".screencap.png")
+    adb.device(serial=deviceAddress).screenshot().save(".screencap.png")
     adb_tap_string.reader = easyocr.Reader(['fr']).readtext(".screencap.png")
     strMatchList = []
     for i in range(0, len(adb_tap_string.reader)):
         if adb_tap_string.reader[i][1] == string:
             strMatchList.append(adb_tap_string.reader[i][0][0])
     os.remove(".screencap.png")
-    stringCordonate = strMatchList[0]
-    adb.device(serial=deviceAddress).click(stringCordonate[0], stringCordonate[1])
-    return ()
-
-def collisee_V2():
-    adb_tap_string("Aventure")
-    time.sleep(4)
-    adb_tap_string("Colisée")
-    time.sleep(4)
-    adb_tap_string("Début bataille")
-    time.sleep(4)
-    adb_tap_string("Début bataille")
-    time.sleep(60)
-    adb_tap_string("Confirmer")
-    time.sleep(4)
-    adb_tap_string("Confirmer")
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(50, 50)
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(50, 50)
+    if len(strMatchList) == 0:
+        return (False)
+    elif len(strMatchList) > 0:
+        return (True)
+def adb_tap_string(string):
+    time.sleep(1)
+    adb.connect(deviceAddress)
+    adb.device(serial=deviceAddress).screenshot().save(".screencap.png")
+    adb_tap_string.reader = easyocr.Reader(['fr']).readtext(".screencap.png")
+    strMatchList = []
+    for i in range(0, len(adb_tap_string.reader)):
+        if adb_tap_string.reader[i][1] == string:
+            strMatchList.append(adb_tap_string.reader[i][0][0])
+    os.remove(".screencap.png")
+    if len(strMatchList) == 0:
+        return ()
+    elif len(strMatchList) > 0:
+        stringCordonate = strMatchList[0]
+        adb.device(serial=deviceAddress).click(stringCordonate[0], stringCordonate[1])
+        return (stringCordonate)
+def adb_tap(coordinates=[0,0]):
+    if coordinates != [0,0]:
+        adb.device(serial=deviceAddress).click(coordinates[0], coordinates[1])
+        return (coordinates)
+    else:
+        return (print("Error this function need coordinates"))
 def guilde_V1():
     adb_tap_string("Guilde")
     time.sleep(2)
@@ -69,16 +75,6 @@ def guilde_V1():
     time.sleep(2)
     adb_tap_string("Confirmer")
     return ()
-
-def dailyV1():
-    guilde_V1()
-    time.sleep(4)
-    donjon()
-    donjon()
-    donjon()
-    getQuestReword()
-
-
 def getQuestReword():
     adb.device(serial=deviceAddress).click(575, 969)
     time.sleep(1)
@@ -92,41 +88,38 @@ def getQuestReword():
     adb.device(serial=deviceAddress).click(50, 50)
 
 def donjon():
-    adb_tap_string("Aventure")
-    time.sleep(2)
-    adb_tap_string("Faille")
-    time.sleep(2)
-    adb_tap_string("Donjon d'éveil")
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(1389, 964)
-    time.sleep(60)
-    adb_tap_string("Sortie")
-    time.sleep(8)
-    adb_tap_string("Confirmer")
-    adb.device(serial=deviceAddress).click(50, 50)
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(50, 50)
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(50, 50)
-    time.sleep(2)
+    loop = True
+    stringsList = ["Aventure", "Faille", "Donjon d'éveil"]
+    for string in stringsList:
+        adb_tap_string(string)
+        time.sleep(2)
+    while loop == True:
+        if adb_find_string("Recharger ticket") == True:
+            loop = False
+        elif adb_find_string("Recharger ticket") == False:
+                adb.device(serial=deviceAddress).click(1389, 964)
+                time.sleep(60)
+                adb_tap_string("Sortie")
+                time.sleep(8)
+                adb_tap_string("Confirmer")
+                time.sleep(4)
+        else:
+            loop = False
+            print("function donjon error")
+    for i in range(3):
+        adb.device(serial=deviceAddress).click(50, 50)
+        time.sleep(2)
 
-def boutique():
+
+def shop():
     adb_tap_string("Menu")
-    time.sleep(2)
-    adb.device(serial=deviceAddress).click(905, 441)
-    time.sleep(2)
-    adb_tap_string("Ressource")
-    time.sleep(2)
-    adb_tap_string("Gratuit")
-    time.sleep(2)
-    adb_tap_string("Gratuit")
-    time.sleep(2)
-    adb_tap_string("Confirmer")
-    time.sleep(2)
-    adb_tap_string("Équipement")
-    time.sleep(2)
-    adb_tap_string("Marteau consolidateur")
-    time.sleep(2)
+    time.sleep(4)
+    adb_tap([909, 430])
+    time.sleep(4)
+    stringsList = ["Ressource", "Gratuit", "Gratuit", "Confirmer", "Équipement", "Marteau consolidateur"]
+    for string in stringsList:
+        adb_tap_string(string)
+        time.sleep(4)
     adb.device(serial=deviceAddress).click(1132, 858)
     time.sleep(2)
     adb_tap_string("Confirmer")
@@ -135,4 +128,37 @@ def boutique():
     time.sleep(2)
     adb.device(serial=deviceAddress).click(630, 443)
     time.sleep(2)
+
+def dailymsg():
+    for i in range(2):
+        if len(adb_tap_string("Recevoir récompense")) > 0:
+            adb_tap_string("Recevoir récompense")
+            if len(adb_tap_string("Confirmer")) > 0:
+                adb_tap_string("Confirmer")
+        elif len(adb_tap_string("Recevoir récompenses")) > 0:
+            adb_tap_string("Recevoir récompenses")
+            if len(adb_tap_string("Confirmer")) > 0:
+                adb_tap_string("Confirmer")
+def collisee_V2():
+    adb_tap_string("Aventure")
+    time.sleep(4)
+    if len(adb_tap_string("Colisée")) > 0:
+        adb_tap_string("Colisée")
+        time.sleep(4)
+    adb_tap([1718,478])
+    time.sleep(2)
+    adb_tap_string("Début bataille")
+    if len(adb_tap_string("Confirmer")) > 0:
+        adb_tap_string("Confirmer")
+        time.sleep(4)
+        adb_tap_string("Confirmer")
+def daily_V2():
+    shop()
+    time.sleep(4)
+    guilde_V1()
+    time.sleep(4)
+    for i in range(3):
+        donjon()
+    getQuestReword()
+
 
---
+++ source/HomePage.py
@@ -0,0 +1,14 @@
+
+
+buttonHero = [150, 700]
+buttonAventure = [150, 900]
+buttonGuilde = [1777, 700]
+buttonInvocations = [1777, 900]
+buttonMenu = [1820, 440]
+
+class Menu:
+    buttonForum = [1820, 440]
+    buttonShop = [1820, 440]
+    buttonQuest = [1820, 440]
+    buttonBook = [1820, 440]
+
